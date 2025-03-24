5d448c668c16de6ec4481770201d7e6619b0074e
Obid Bozorov <bozorov.obid@gmail.com>
2025-03-24 23:17:23 +0500

    2-dars

---
+++ 6. Funksiyalar/Funksiyalar.py
@@ -0,0 +1,34 @@
+# def Display():
+#     print("Python tilida funksiya e`loni!")
+# Display()
+# Display()
+# def Salom(ismi,familiyasi, sharifi):
+#     print("Salom",familiyasi,ismi, sharifi)
+# Salom("Tolib","Qodirov","Komil o'g'li")
+# Salom(familiyasi="Shonazarov", ismi="Shoxijahon",sharifi="Xusain o'g'li")
+# ismi = "Sardor"
+# familiyasi="Qodirov"
+# def Salom():
+#     global familiyasi;
+#     familiyasi="Komilov"
+#     print("Salom", ismi)
+#     print(familiyasi)
+# def Xayr():
+#     print("Xayr", familiyasi)
+# Salom()
+# Xayr()
+# def talabani_tanishtir(FISH,kursi, Yunalishi, Millati, Universiteti)  :
+#     print(f"Tabaning FISH: {FISH}; \n kursi: {kursi};\n Yunalishi: {Yunalishi}; \n Millati: {Millati}; \n Universiteti: {Universiteti}. ")
+# # talabani_tanishtir("Qodirov Qodir",4,"AX","O'zbek","O'zMU")
+# def talabani_tanishtir(FISH,kursi, Yunalishi, Millati="O'zbek", Universiteti="O'zMU")  :
+#     print(f"Tabaning FISH: {FISH}; \n kursi: {kursi};\n Yunalishi: {Yunalishi}; \n Millati: {Millati}; \n Universiteti: {Universiteti}. ")
+# talabani_tanishtir("Qodirov Qodir",4,"AX",Universiteti="TATU")
+# talabani_tanishtir("Qodirov Qodir",4,"AX","TATU")
+def katta_kichik(ruyxat):
+    return max(ruyxat),min(ruyxat)
+
+def kupaytma(a,b):
+    return a*b
+# a=[4,5,6,0,12,5,45,6,54,6,-6,2,61,66,1,1,3,65]
+# #maksimum_a,minumum_a=maksimum(a)
+# #print(maksimum_a,minumum_a)
---
+++ 6. Funksiyalar/Ruzmatov.py
@@ -0,0 +1,10 @@
+#import Funksiyalar
+import numpy
+import Funksiyalar as yordamchi_funksiyalar
+from  Funksiyalar import *
+import dars.Istisnoholatlar
+# Funksiyalar import katta_kichik,kupaytma
+baholarim=[3,4,5,0,2]
+eng_yuqori_bahoyim, eng_quyi_bahoyim=yordamchi_funksiyalar.katta_kichik(ruyxat=baholarim)
+eng_yuqori_bahoyim, eng_quyi_bahoyim=katta_kichik(ruyxat=baholarim)
+print(eng_quyi_bahoyim)
